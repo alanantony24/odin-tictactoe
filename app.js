a5f432a8a7fb66f9a9bf6af56757205fca712a07
alanantony24 <s10207327@connect.np.edu.sg>
2023-09-30 23:22:43 +0800

    done

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+const displayController = (() => {
+  const renderMessage = (message) => {
+    document.querySelector("#message").innerHTML = message;
+  };
+  return {
+    renderMessage,
+  };
+})();
+
 const Gameboard = (() => {
   let gameboard = ["", "", "", "", "", "", "", "", ""];
   const render = () => {
@@ -22,7 +31,7 @@ const Gameboard = (() => {
   return {
     render,
     update,
-    getGameBoard
+    getGameBoard,
   };
 })();
 
@@ -58,14 +67,67 @@ const Game = (() => {
       return;
     }
     Gameboard.update(index, players[currentPlayerIndex].mark);
+
+    if (
+      checkForWin(Gameboard.getGameBoard(), players[currentPlayerIndex].mark)
+    ) {
+      gameOver = true;
+      displayController.renderMessage(
+        `${players[currentPlayerIndex].name} wins`
+      );
+    } else if (checkForTie(Gameboard.getGameBoard())) {
+      gameOver = true;
+      displayController.renderMessage("It's a tie");
+    }
+
     currentPlayerIndex = currentPlayerIndex === 0 ? 1 : 0;
+  };
+
+  const restart = () => {
+    for (let i = 0; i < 9; i++) {
+      Gameboard.update(i, "");
+    }
+    Gameboard.render();
+    gameOver = false;
+    document.querySelector("#message").innerHTML = "";
   };
 
   return {
     start,
+    restart,
     handleClick,
   };
 })();
+
+function checkForWin(board) {
+  const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < winningCombinations.length; i++) {
+    const [a, b, c] = winningCombinations[i];
+    if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function checkForTie(board) {
+  return board.every((cell) => cell !== "");
+}
+
+const restartButton = document.querySelector("#restart-button");
+restartButton.addEventListener("click", () => {
+  Game.restart();
+});
 
 const startButton = document.querySelector("#start-button");
 startButton.addEventListener("click", () => {
